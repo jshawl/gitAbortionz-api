134027261bcfc25fbb4d8fd73e6ea33b4596d59d
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-01 16:53:41 -0400

    updated seeds.rb State typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 policies = JSON.parse(File.read("db/states_policies.json"))
 data = JSON.parse(File.read("db/states_data.json"))
 
-Product.destroy_all
+State.destroy_all
 
-Product.create!(policies)
+State.create!(policies)
 
-Product.create!(data)
+State.create!(data)
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
