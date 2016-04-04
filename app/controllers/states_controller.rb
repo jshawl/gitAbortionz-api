c54cc8fb39e99ddb29c4eb28cacfd3948053603e
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-04 15:19:54 -0400

    started fleshing out states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,10 +1,16 @@
-class StatesController < ActionController::Base
+class StatesController < ApplicationController
 
 def index
+  @states = State.all
   respond_to do |format|
     format.html
     format.json{render json: State.all}
   end
+end
+
+def show
+  @state = State.find(params[:state_id])
+  # render json: @state, status: :ok
 end
 
 end
---
+++ app/views/states/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @state, :state_id, :measure_id, :measure_name, :guttmacher_id, :datum, :datum_date, :first_year, :last_year, :footnotes, :sources
