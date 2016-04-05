4b8dbd49ffea8edc2cb303710fa8f94841b8dbda
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-04-05 15:23:08 -0400

    fixed template

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,17 +1,12 @@
 //=require angularjs
 //=require angular-resource
-<<<<<<< HEAD
 
-
-=======
->>>>>>> c6d36e172db4699b4958a2a7ec3c2969d2050e36
 "use strict";
 (function(){
   angular
   .module("abortion", [
-    "ui.router",
-    "states",
-    "posts"
+  "ui.router",
+  "states"
   ])
   .config([
     "$stateProvider",
@@ -19,23 +14,13 @@
   ]);
 
   function RouterFunction($stateProvider){
-<<<<<<< HEAD
-    $stateProvider
-    .state("StateIndex", {
-      url: "/states",
-      templateUrl: "/assets/states/index.html",
-      controller: "stateIndexController",
-      controllerAs: "StateIndexViewModel"
-    });
-=======
 $stateProvider
 .state("StateIndex", {
   url: "/states",
-  templateUrl: "ng-views/states/index.html",
+  templateUrl: "/",
   controller: "stateIndexController",
   controllerAs: "StateIndexViewModel"
 });
->>>>>>> badee66a65f2e6ff0857588f70a955a812119126
   }
 
 })();
