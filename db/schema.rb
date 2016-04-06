ee2bcb429e0851d016555b15f85c5533b04b4bcd
Fatai Agiri <yinkaagiri@gmail.com>
2016-04-06 17:03:22 -0400

    Revert "Fatai comments"

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406193336) do
+ActiveRecord::Schema.define(version: 20160405221633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "author"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.integer  "post_id"
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
@@ -57,5 +47,4 @@ ActiveRecord::Schema.define(version: 20160406193336) do
     t.string  "two_trips"
   end
 
-  add_foreign_key "comments", "posts"
 end
