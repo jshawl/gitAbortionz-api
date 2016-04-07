f5bb59ef6f834cfe51a9697105b46f08aaf37924
yinkaagiri <yinka.fatai@gmail.com>
2016-04-06 20:04:45 -0400

    added more methods in comments controller, deleted comment/index.html

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,36 @@ class CommentsController < ActionController::Base
 def index
   @post = Post.find(params[:post_id])
   @comments = @post.comments
-render json: @post.comments
+render json: @post.comments.all
 end
+
+def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+  end
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
 end
